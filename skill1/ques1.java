d31368d35e775afdb1d01ca7c9d5eb7d89e3186f
harshini-patibandla <harshinipatibandla2006@gmail.com>
2025-08-06 15:31:52 +0530

    Add files via upload

---
+++ skill1/ques1.java
@@ -0,0 +1,16 @@
+package skill1;
+
+public class ques1 {
+
+
+    public static void main(String[] args) {
+        int numInt = 100;
+        long numLong = numInt;    
+        float numFloat = numLong;    
+
+        System.out.println("Integer value: " + numInt);
+        System.out.println("Long value: " + numLong);
+        System.out.println("Float value: " + numFloat);
+    }
+}
+
---
+++ skill1/ques2.java
@@ -0,0 +1,10 @@
+package skill1;
+
+public class ques2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
